5c48a0422613eacb6270f39cd316e0a0f7238d95
hbill75 <shillcisco@gmail.com>
2019-10-09 23:58:36 -0400

    reverted to django 1.11.23 and fixed create_users.py

--- demos/django/create_users.py
+++ demos/django/create_users.py
@@ -1,8 +1,7 @@
 import os
-import django
-from django.contrib.auth.models import User
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_site.settings")
+import django
 django.setup()
+from django.contrib.auth.models import User
 user = User.objects.create_user('user1', 'user1@mycompanymail.xyz', 'C1sco12345')
 user = User.objects.create_user('user2', 'user2@mycompanymail.xyz', 'C1sco12345')
